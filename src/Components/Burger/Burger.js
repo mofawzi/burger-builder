54658ba42dbcf68e5c091b64672af66bc7a3feb1
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-08 01:43:37 +0200

    Output dynamic ingrdients

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,12 +3,18 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
 
 const burger = (props) => {
+  // Transform the object (key, value) to be an array of ingredients ' where the key is the ingredient , the value is the qiantity'
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingKey) => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    }
+  );
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bacon" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
