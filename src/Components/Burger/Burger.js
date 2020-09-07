b0a0334655e2ea511eeea88cd84efd58e27b26d6
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-08 01:50:59 +0200

    Check for zero ingredients added

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,13 +4,21 @@ import classes from "./Burger.css";
 
 const burger = (props) => {
   // Transform the object (key, value) to be an array of ingredients ' where the key is the ingredient , the value is the qiantity'
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    (ingKey) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingKey) => {
       return [...Array(props.ingredients[ingKey])].map((_, i) => {
         return <BurgerIngredient key={ingKey + i} type={ingKey} />;
       });
-    }
-  );
+    })
+    // Use reduce to get the actual number of ingredients -> Not the array length
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>PLease start adding ingredients</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
