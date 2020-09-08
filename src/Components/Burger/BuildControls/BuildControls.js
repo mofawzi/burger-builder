63370283e283e97b8f512d939db8293735922ffe
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-08 02:08:44 +0200

    Add Build Control style

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,5 @@
 import React from "react";
 import classes from "./BuildControls.css";
 
-const buildControls = (props) => <div className={classes.buildControls}></div>;
+const buildControls = (props) => <div className={classes.BuildControls}></div>;
 export default buildControls;
