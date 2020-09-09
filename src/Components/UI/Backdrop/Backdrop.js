8ff954453fdbf57b4c6730eacc872f2b0c32e9ff
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-09 06:34:05 +0200

    Add Backdrop styling

--- src/Components/UI/Backdrop/Backdrop.js
+++ src/Components/UI/Backdrop/Backdrop.js
@@ -2,6 +2,8 @@ import React from "react";
 import classes from "./Backdrop.css";
 
 const backdrop = (props) =>
-  props.show ? <div className={classes.Backdrop}></div> : null;
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked}></div>
+  ) : null;
 
 export default backdrop;
