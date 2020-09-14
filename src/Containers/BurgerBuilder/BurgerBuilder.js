05cbac6b483ee206efa5d4eacf3d495baf8d2d59
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-14 14:23:18 +0200

    Send orders to database

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../Components/Burger/Burger";
 import BuildControls from "../../Components/Burger/BuildControls/BuildControls";
 import Modal from "../../Components/UI/Modal/Modal";
 import OrderSummary from "../../Components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 // Price Set
 const INGREDIENT_PRICES = {
@@ -91,7 +92,28 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    // Creating an object to be stored in DB
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Mohamed Fawzy",
+        adress: {
+          stret: "Saif 603",
+          zipCode: "12345",
+          city: "Alexandria",
+          country: "Egypt",
+        },
+        email: "test@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+
+    // Send a post request to save the order in DB
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   };
 
   render() {
