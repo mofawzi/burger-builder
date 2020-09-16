abbb42bb0cfcb2e6494982c900edd66b8429a41e
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-16 07:00:21 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import classes from "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 import Layout from "./Components/Layout/Layout";
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./Containers/Checkout/Checkout";
 
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
