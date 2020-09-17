10f79ce301a350e61a1641631f4e86d03dacc039
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-17 09:52:45 +0200

    Add initial contact data

---
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import Button from "../../../Components/UI/Button/Button";
+
+class ContactData extends Component {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      city: "",
+    },
+  };
+  render() {
+    return (
+      <div>
+        <h4>Enter your Data</h4>
+        <form>
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="email" name="email" placeholder="Your Email" />
+          <input type="text" name="city" placeholder="Your City" />
+          <input type="text" name="Street" placeholder="Your street" />
+          <Button btnType="Success">ORDER</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactData;
