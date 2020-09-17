6f0dd1eb1acb8f9ac4bc080e7dab3f71741f7746
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-17 10:09:28 +0200

    Update contact data styling

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -14,12 +14,32 @@ class ContactData extends Component {
   render() {
     return (
       <div className={classes.ContactData}>
-        <h4>Enter your Data</h4>
+        <h4>Enter your Contact Data</h4>
         <form>
-          <input type="text" name="name" placeholder="Your Name" />
-          <input type="email" name="email" placeholder="Your Email" />
-          <input type="text" name="city" placeholder="Your City" />
-          <input type="text" name="Street" placeholder="Your street" />
+          <input
+            className={classes.Input}
+            type="text"
+            name="name"
+            placeholder="Your Name"
+          />
+          <input
+            className={classes.Input}
+            type="email"
+            name="email"
+            placeholder="Your Email"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="city"
+            placeholder="Your City"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="Street"
+            placeholder="Your street"
+          />
           <Button btnType="Success">ORDER</Button>
         </form>
       </div>
