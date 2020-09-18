edb65dca0ae9f28cf3d885e100d5b3d070d953b8
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-18 14:53:20 +0200

    Update orders page

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -2,11 +2,36 @@ import React from "react";
 import classes from "./Order.css";
 
 const order = (props) => {
+  // Collect the order
+  const ingredients = [];
+  // Map ingredients into an array
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
+  }
+  const ingredientOutput = ingredients.map((ing) => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          padding: "5px ",
+          border: "1px solid #ccc",
+        }}
+        key={ing.name}
+      >
+        {ing.name} ( {ing.amount} )
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
+      <p>Ingredients: {ingredientOutput} </p>
       <p>
-        Price: <strong>USD {props.price}</strong>
+        Price: <strong>USD {props.price.toFixed(2)}</strong>
       </p>
     </div>
   );
