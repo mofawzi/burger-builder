7e6f069542d4abc0f261424c0416a9dc43d31429
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-09-20 10:13:37 +0200

    Update app logic

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux ";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./store/reducer";
 
-// creaate the Store (Global state) for redux
+// Create the Store (Global state) for redux
 const store = createStore(reducer);
 
 // Enable Routing and Redux
@@ -21,8 +21,4 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
