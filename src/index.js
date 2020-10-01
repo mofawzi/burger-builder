897459ed3e79f0642c0f680cb0be69c68c39f0fd
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-10-01 16:03:30 +0200

    Add middleware setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+
+// Create enhancer for Async code
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Create the Store (Global state) for redux
 const store = createStore(
   burgerBuilderReducer,
-  // Add redux developer tool extention
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // Add redux developer tool extention, appying middleware
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 // Enable Routing and Redux
