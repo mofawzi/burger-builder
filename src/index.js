0cbc2511b91178013f77ecba6c9ac60d2433844c
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-10-01 15:27:28 +0200

    Enable Redux dev tool

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ import { createStore } from "redux";
 import reducer from "./store/reducer";
 
 // Create the Store (Global state) for redux
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  // Add redux developer tool extention
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // Enable Routing and Redux
 const app = (
