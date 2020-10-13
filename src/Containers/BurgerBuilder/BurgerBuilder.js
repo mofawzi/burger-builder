87b6d89d7bd90167d8fc340787445edf933b7932
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-10-13 22:28:37 +0200

    Combine Reducers

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -112,9 +112,9 @@ class BurgerBuilder extends Component {
 // Handling Redux
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    error: state.error,
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    error: state.burgerBuilder.error,
   };
 };
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
 
-// Create enhancer for Async code
+// Create enhancers for Async code
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Combine Reducers
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+});
 
 // Create the Store (Global state) for redux
 const store = createStore(
-  burgerBuilderReducer,
+  rootReducer,
   // Add redux developer tool extention, appying middleware
   composeEnhancers(applyMiddleware(thunk))
 );
