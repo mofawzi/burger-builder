d077cf6cb7cb169a6596a1156397b7040644699e
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-10-19 01:33:41 +0200

    Add auth actions

---
+++ src/store/actions/auth.js
@@ -0,0 +1,25 @@
+import * as actionTypes from "./actionTypes";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
+
+export const authFail = (err) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: err,
+  };
+};
+
+export const auth = (email, password) => (dispatch) => {
+  dispatch(authStart);
+};
