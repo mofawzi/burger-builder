010529dc8f87f877a569b922c5cf903fa3e3d71c
Mohamed Fawzy <mofawzy1996@gmail.com>
2020-11-16 20:55:04 +0200

    Using Environment Variables

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 
 // Create enhancers for Async code
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 // Combine Reducers
 const rootReducer = combineReducers({
